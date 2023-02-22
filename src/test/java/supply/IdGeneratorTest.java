07a937f3702617cac4c7f217fc5a12a6e5c52dfb
bradley4 <yesyesyes@hotmail.co.kr>
2023-02-22 12:07:46 +0900

    git init

---
+++ src/test/java/supply/IdGeneratorTest.java
@@ -0,0 +1,9 @@
+package supply;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class IdGeneratorTest {
+
+}
